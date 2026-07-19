6f22df4dd5436127cc6c2ddab23afb86bdb48865
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-19 13:42:04 +0200

    refactor: apps to use event instead of closure for conditional navigation entries
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- core/AppInfo/Application.php
+++ core/AppInfo/Application.php
@@ -21,6 +21,7 @@ use OC\Authentication\Notifications\Notifier as AuthenticationNotifier;
 use OC\Core\Listener\AddMissingIndicesListener;
 use OC\Core\Listener\AddMissingPrimaryKeyListener;
 use OC\Core\Listener\BeforeTemplateRenderedListener;
+use OC\Core\Listener\LoadAdditionalEntriesListener;
 use OC\Core\Listener\PasswordUpdatedListener;
 use OC\Core\Listener\RestrictInteractionListener;
 use OC\Core\Notification\CoreNotifier;
@@ -35,11 +36,8 @@ use OCP\AppFramework\Http\Events\BeforeLoginTemplateRenderedEvent;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 use OCP\DB\Events\AddMissingIndicesEvent;
 use OCP\DB\Events\AddMissingPrimaryKeyEvent;
-use OCP\INavigationManager;
 use OCP\Interaction\RestrictInteractionEvent;
-use OCP\IURLGenerator;
-use OCP\IUserSession;
-use OCP\L10N\IFactory;
+use OCP\Navigation\Events\LoadAdditionalEntriesEvent;
 use OCP\User\Events\BeforeUserDeletedEvent;
 use OCP\User\Events\PasswordUpdatedEvent;
 use OCP\User\Events\UserDeletedEvent;
@@ -76,6 +74,7 @@ class Application extends App implements IBootstrap {
 		$context->registerEventListener(AddMissingPrimaryKeyEvent::class, AddMissingPrimaryKeyListener::class);
 		$context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
 		$context->registerEventListener(BeforeLoginTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
+		$context->registerEventListener(LoadAdditionalEntriesEvent::class, LoadAdditionalEntriesListener::class);
 		$context->registerEventListener(RemoteWipeStarted::class, RemoteWipeActivityListener::class);
 		$context->registerEventListener(RemoteWipeStarted::class, RemoteWipeNotificationsListener::class);
 		$context->registerEventListener(RemoteWipeStarted::class, RemoteWipeEmailListener::class);
@@ -104,34 +103,5 @@ class Application extends App implements IBootstrap {
 
 	#[\Override]
 	public function boot(IBootContext $context): void {
-		$context->injectFn($this->registerNavigationEntries(...));
-	}
-
-	/**
-	 * Registers the navigation entries for the core app:
-	 * - The logout button in the settings menu
-	 */
-	public function registerNavigationEntries(
-		INavigationManager $navigationManager,
-		IUserSession $userSession,
-		IURLGenerator $urlGenerator,
-		IFactory $factory,
-	): void {
-		if (!$userSession->isLoggedIn()) {
-			return;
-		}
-
-		$l = $factory->get('core');
-
-		// Register the logout button in the user settings
-		$logoutUrl = $urlGenerator->getLogoutUrl();
-		$navigationManager->add([
-			'type' => 'settings',
-			'id' => 'logout',
-			'order' => 99999,
-			'href' => $logoutUrl,
-			'name' => $l->t('Log out'),
-			'icon' => $urlGenerator->imagePath('core', 'actions/logout.svg'),
-		]);
 	}
 }
